e5ad3460b9b5cc4bfbfc37bdbaeecb8fd4f63c1a
Nikita Kolmogorov <backmeupplz@gmail.com>
2018-06-04 10:08:15 -0700

    fix syntax

--- index.js
+++ index.js
@@ -4,37 +4,26 @@ require('dotenv').config({path: `${__dirname}/.env`})
 const Telegraf = require('telegraf')
 
 // Setup the bot
-const bot = new Telegraf(process.env.TOKEN)
-
+const bot = new Telegraf(process.env.TOKEN, {username: 'deletr_bot'})
 // Setup help message
 const help = 'Hello there! I\'m @deletr_bot and my sole purpose is to delete <code>Deleted Account</code> users from chats and channels. To unleash my full potential, add me to a channel or chat, make me an admin in that channel or chat and then simply write /delete there. That\'s it! No more filthy <code>Deleted Account</code> users. You are welcome.\n\n/help — This message\n/delete — delete all <code>Deleted Account</code> users from this group or chat'
-
-// Custom middleware
-bot.use((ctx, next) => {
-  // If there is no message, just do next
-  if (!ctx.message) return next()
-  // Add to chat
-  if (ctx.message.new_chat_member && ctx.message.new_chat_member.username === 'deletr_bot')
-    return sendHelp(ctx)
-  // Start and help commands
-  const isCommand = ctx.message.entities &&
-    ctx.message.entities.length &&
-    ctx.message.entities.map(e => e.type).includes('bot_command')
-  const regex_results = /\/([^@\s]{1,})(?:@deletr_bot|\s|$)/gm.exec(ctx.message.text)
-  const command = !regex_results ? null : regex_results[1]
-  if (command === 'delete')
-    deleteUsers(ctx)
-  else if (command === 'start' || command === 'help')
+// Added to the chat
+bot.on('new_chat_members', (ctx) => {
+  if (ctx.map(m => m.username).includes('deletr_bot'))
     sendHelp(ctx)
-  // Continue execution
-  return next()
 })
-
+// Start and help commands
+bot.command(['start', 'help'], (ctx) => {
+  sendHelp(ctx)
+})
+// Delete command
+bot.command('delete', (ctx) => {
+  deleteUsers(ctx)
+})
 // Send help function
 function sendHelp(ctx) {
   ctx.replyWithHTML(help)
 }
-
 // Delete
 async function deleteUsers(ctx) {
   // Check if this is a private chat
@@ -52,10 +41,9 @@ async function deleteUsers(ctx) {
     return
   }
   // Notify users that delete operation has started
-  ctx.replyWithMarkdown('Deleting all `Deleted Account` users...')
+  ctx.replyWithMarkdown('Deleting all `Deleted Account` users... (tbh, not working yet)')
   // Get all users
   // TODO: implement when Telegram Bot Api allows us to fetch users list
 }
-
 // Start the bot
 bot.startPolling()
